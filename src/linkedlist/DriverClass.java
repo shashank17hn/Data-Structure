62fc2b4bde14e97a81bb50b88c8807e3d1a18517
Shashank Shivhare <shashank17hn@gmail.com>
2018-07-04 00:47:35 -0400

    merge linkedlist

--- src/linkedlist/DriverClass.java
+++ src/linkedlist/DriverClass.java
@@ -6,7 +6,8 @@ public class DriverClass {
 		// TODO Auto-generated method stub
 		
 	  LinkedList list = new LinkedList();
-	  list.appendLast(4);
+	  LinkedList list2 = new LinkedList();
+/*	  list.appendLast(4);
 	  list.appendLast(2);
 	  list.appendLast(3);
 	  list.appendFirst(1);
@@ -28,7 +29,24 @@ public class DriverClass {
 	  
 	  System.out.println("----------------------");
 	  list.deleteAt(0);
+	  list.showList();*/
+	  
+	  System.out.println("----------------------");
+	  list.appendLast(1);
+	  list2.appendLast(2);
+	  list.appendLast(3);
+	  list2.appendLast(4);
+	  list2.appendLast(5);
+	 // list.appendFirst();
 	  list.showList();
+	  System.out.println("----------------------");
+	  list2.showList();
+	  System.out.println("----------------------");
+	  list.mergeSortedLinkedList(list.head, list2.head);
+	  
+	/*//  list.sortLinkedList();
+	  System.out.println("----------------------");
+	  list.showList();*/
 	}
 
 }
--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -72,6 +72,73 @@ public class LinkedList {
 	}
 	
 	
+	public void sortLinkedList() {
+		
+		Node current = head;
+		Node current2 = head.next;
+		
+		while(current.next!= null) {
+			
+			while(current2.next!= null) {
+				if(current.next.data >= current2.next.data) {
+					System.out.println(current.next.data  + ":" + current2.next.data );
+					Node temp = current.next;
+					current.next = current2.next;
+					current2.next =temp;
+					
+				}
+				current2 = current2.next;
+				
+			}
+			System.out.println(current.next.data  + ":" + current2.next.data );
+			current = current.next;
+		}
+		
+	}
+	
+	public void mergeSortedLinkedList(Node n1, Node n2) {
+		
+		Node head1 =  new Node(0);
+		Node node3 = head1;
+		while(n1 != null || n2 != null) {
+			if(n1 != null && n2 != null) {
+				if(n1.data > n2.data) {
+					node3.next = n2;
+					n2 = n2.next;
+				}
+				else {
+					node3.next = n1;
+					n1 = n1.next;
+				}
+				//node3 = node3.next;
+		}
+			else {
+				if(n1 == null) {
+					node3.next = n2;
+					n2 = n2.next;
+				}else {
+					node3.next = n1;
+					n1 = n1.next;
+				}
+				//node3 = node3.next;
+			}
+			
+		//	System.out.println("liste ready");
+			node3 = node3.next;
+		}
+		System.out.println("liste ready");
+		showListMergedList(head1.next);
+	}
+	
+	
+	public void showListMergedList(Node head1) {
+		Node current = head1;
+		System.out.println(current.data);
+		while(current.next != null) {
+			System.out.println(current.next.data);
+			current = current.next;
+		}
+	}
 	
 	public void showList() {
 		Node current = head;
